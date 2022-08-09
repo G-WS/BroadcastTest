9d867b5ac15556e201e0a56b9b649dab7bd60606
G-WS <3355139577@qq.com>
2022-08-09 11:47:46 +0800

    Initial commit

--- app/src/main/java/com/example/broadcasttest/MainActivity.java
+++ app/src/main/java/com/example/broadcasttest/MainActivity.java
@@ -9,32 +9,46 @@ import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private IntentFilter intentFilter;
 
     private NetworkChangeReceiver networkChangeReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.example.broadcasttest.MY_BROADCAST");
+                intent.setPackage("com.example.broadcasttest");//需添加此行才可以保证自定义广播的正常的执行
+//                sendBroadcast(intent);//标准标准
+//                intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+                sendOrderedBroadcast(intent, null);
+            }
+        });
         intentFilter = new IntentFilter();
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
-        networkChangeReceiver  = new NetworkChangeReceiver();
-        registerReceiver(networkChangeReceiver,intentFilter);
+        networkChangeReceiver = new NetworkChangeReceiver();
+        registerReceiver(networkChangeReceiver, intentFilter);
     }
-    class NetworkChangeReceiver extends BroadcastReceiver{
+
+    class NetworkChangeReceiver extends BroadcastReceiver {
 
         @Override
         public void onReceive(Context context, Intent intent) {
             ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
             NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-            if (networkInfo!=null&& networkInfo.isAvailable()){
-                Toast.makeText(context,"network is available",Toast.LENGTH_SHORT).show();
-            }else{
-                Toast.makeText(context,"network is unavailable",Toast.LENGTH_SHORT).show();
+            if (networkInfo != null && networkInfo.isAvailable()) {
+                Toast.makeText(context, "network is available", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT).show();
 
             }
         }
